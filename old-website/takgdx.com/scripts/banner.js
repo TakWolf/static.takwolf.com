cb57a6e9436eeebaadeccff0f47c3db64be950bc
TakWolf <takwolf@foxmail.com>
2020-11-03 23:37:44 +0800

    a

---
+++ old-website/takgdx.com/scripts/banner.js
@@ -0,0 +1,29 @@
+//取出canvas对象
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+			
+//设置canvas尺寸
+canvas.width = 100;
+canvas.height = 360;
+
+
+
+//切换到下一页
+var nextPage = function()
+{
+	
+	
+}
+
+//切换到上一页
+var lastPage = function()
+{
+
+}
+
+
+var main = function()
+{
+	ctx.lineTo(106.333,111.333); 
+}
+setInterval(main, 1); 
---
+++ trd/Love-Page/js/console.js
@@ -0,0 +1,8 @@
+if (window.console) {
+    var cons = console;
+    if (cons) {
+        cons.log("%c\n      ", "font-size:60px;background:url('http://lovejiajia.com/icon/ilogo.png') no-repeat no-repeat");
+        cons.log('在这个日子里，我只想对你、对你们说：今生能遇见你们真的很好。\n也许每个人都有每个人的苦，每个人都有每个人的泪，但即便是擦肩而过的美好，也值得我们深深的记住。\n谢谢你们。\n曾今，现在，将来，如果遇到生命中对的那个人，请一定要好好把握呢！');
+        cons.log("Copyleft 2013 10 %c❤", "color:rgb(246,105,147);font-weight:bold;");
+    }
+}
